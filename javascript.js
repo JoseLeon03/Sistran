8bb0985182a6a811f305ba9a99f7efb2ea639bed
JoseLeon03 <73024808+JoseLeon03@users.noreply.github.com>
2023-05-30 09:59:02 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,19 @@
-function Cerrar() {
+function limpiarCampos() {
     document.getElementById("username").value = "";
     document.getElementById("password").value = "";
-  }
+}
 
-function validarFormulario() {
+function Validar() { 
     var usuario = document.getElementById("username").value;
     var contraseña = document.getElementById("password").value;
-  
     if (usuario == "") {
-      alert("Por favor, ingrese el usuario.");
-      return false;
-    }
-    if ( contraseña == "") {   
-        alert( "Por favor, ingrese la contraseña,");
+        alert("Por favor, Ingrese un usuario");
         return false;
-      }
-    else {
-      return true;
     }
-  }
-  
+    else if (contraseña == "") {
+        alert("Por favor, Ingrese una contraseña")
+    }
+    else {
+        return true;
+    }
+}
