485bf1eef8e4d46f25945ef745b08af45c9c6d46
JoseLeon03 <73024808+JoseLeon03@users.noreply.github.com>
2023-05-30 20:51:54 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,51 @@
-function limpiarCampos() {
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
+const sql = require('mssql');
+
+const config = {
+ 
+  server: 'DESKTOP-8VVN798',
+  database: 'Sistran',
+  options: {
+    trustedConnection: true
+  }
+};
+
+async function conectar() {
+  try {
+    await sql.connect(config);
+    console.log('Conexión exitosa a la base de datos');
+
+    const BotonLogin = document.getElementById('Login');
+    const BotonLimpiar = document.getElementById('Limpiar');
+    const BotonSalir = document.getElementById('Salir');
+    const userField = document.getElementById('username');
+    const passwordField = document.getElementById('password');
+
+    BotonLogin.addEventListener('click', async function() {
+      const username = userField.value;
+      const password = passwordField.value;
+
+      const resultado = await sql.query(`SELECT COUNT(*) AS total FROM usuarios WHERE usuario='${username}' AND contraseña='${password}'`);
+
+      if (resultado.recordset[0].total > 0) {
+        console.log(`Ingresando con usuario ${username} y contraseña ${password}`);
+        window.location.href = 'FormularioContacto - copia.html';
+      }   else {
+        console.log('Nombre de usuario o contraseña incorrectos');
+        alert('Nombre de usuario o contraseña incorrectos');
+      }
+    });
+
+    BotonLimpiar.addEventListener('click', function() {
+      userField.value = '';
+      passwordField.value = '';
+    });
+
+   BotonSalir.addEventListener('click', function() {
+      window.close();
+    });
+  } catch (error) {
+    console.log('Error al conectar a la base de datos', error);
+  }
 }
 
-function Validar() { 
-    var usuario = document.getElementById("username").value;
-    var contraseña = document.getElementById("password").value;
-    if (usuario == "") {
-        alert("Por favor, Ingrese un usuario");
-        return false;
-    }
-    else if (contraseña == "") {
-        alert("Por favor, Ingrese una contraseña")
-    }
-    else {
-        return true;
-    }
-}
+conectar();
